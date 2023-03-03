688a5451a55ee4a58af6ccb4be7bb1b145bf2109
Farhod Abdusaidov <125345656+Farkhod0608@users.noreply.github.com>
2023-03-03 10:38:01 +0500

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,14 @@
+# loops
+If/else loops practice 
+
+# python junior 
+
+
+#create a list called 'cars'
+
+cars = ['toyota','mazda','ferrari','bmw','audi']
+for car in cars:
+  if car == 'bmw': # if a car's name is 'bmw'
+    print (car.upper()) #it will print all the name with capital letters 
+  else: #otherwise only the first letter will be capital 
+    print (car.title())
